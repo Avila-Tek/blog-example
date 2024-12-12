ab1a42d54ca1fcf0b69acffbaa51f940d91eedd1
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2024-12-12 15:50:54 -0400

    feat(admin): :sparkles: sidebar layout

---
+++ apps/admin/src/components/app-sidebar.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+} from 'lucide-react';
+import * as React from 'react';
+
+import { NavMain } from '@/components/nav-main';
+import { NavProjects } from '@/components/nav-projects';
+import { NavUser } from '@/components/nav-user';
+import { TeamSwitcher } from '@/components/team-switcher';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from '@/components/ui/sidebar';
+import { TUser } from '@repo/schemas';
+
+// This is sample data.
+const data = {
+  teams: [
+    {
+      name: 'Acme Inc',
+      logo: GalleryVerticalEnd,
+      plan: 'Enterprise',
+    },
+  ],
+  navMain: [
+    {
+      title: 'Users',
+      url: '/users',
+      icon: SquareTerminal,
+    },
+    {
+      title: 'Blog Posts',
+      url: '/posts',
+      icon: BookOpen,
+    },
+  ],
+};
+
+type TSideSidebarProps = React.ComponentProps<typeof Sidebar> & {
+  user: TUser | undefined | null;
+};
+
+export async function AppSidebar({ user, ...props }: TSideSidebarProps) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
