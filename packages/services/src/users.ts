f4a2b204adab4037a352fe9a13b58a2a16159924
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2024-12-17 17:22:26 -0400

    feat(packages/services): :sparkles: update user

--- packages/services/src/users.ts
+++ packages/services/src/users.ts
@@ -1,7 +1,9 @@
 import {
   Pagination,
   TCreateUserInput,
+  TFindOneUserInput,
   TPaginationInfo,
+  TUpdateUserInput,
   TUser,
   createPaginationSchema,
   userSchema,
@@ -56,4 +58,49 @@ async function paginationUser(
   return parseResponse;
 }
 
-export const userService = Object.freeze({ createUser, paginationUser });
+async function findOneUser(
+  input: Pick<TFindOneUserInput, '_id'>,
+  config: Omit<TRequestConfig, 'url'>
+): Promise<Safe<TUser>> {
+  const response = await safeFetch(
+    new URL(`${config.baseUrl}/api/v1/users/${input._id}`),
+    {
+      ...(config ?? {}),
+      method: 'GET',
+    }
+  );
+  if (!response.success) {
+    return response;
+  }
+  const parseResponse = safe(() => userSchema.parse(response.data));
+  return parseResponse;
+}
+
+async function updateOneUser(
+  input: TUpdateUserInput,
+  config: Omit<TRequestConfig, 'url'>
+): Promise<Safe<TUser>> {
+  const response = await safeFetch(
+    new URL(`${config.baseUrl}/api/v1/users/${input._id}`),
+    {
+      ...(config ?? {}),
+      body: JSON.stringify(input),
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+  if (!response.success) {
+    return response;
+  }
+  const parseResponse = safe(() => userSchema.parse(response.data));
+  return parseResponse;
+}
+
+export const userService = Object.freeze({
+  createUser,
+  paginationUser,
+  findOneUser,
+  updateOneUser,
+});
